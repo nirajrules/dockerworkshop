882d72e4054d1b45e6077af6241c0ed5ae74d9a5
Niraj <niraj.rules@gmail.com>
2019-11-20 04:55:08 +0000

    code update

---
+++ 08Microservices/webapplication/src/SampleIISApp/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SampleIISApp
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var apiUrl = Environment.GetEnvironmentVariable("API_URL",
+EnvironmentVariableTarget.Machine);
+            if (!string.IsNullOrEmpty(apiUrl))
+            {
+                var request = WebRequest.Create(apiUrl);
+                using (var response = request.GetResponse())
+                using (var responseStream = new
+                StreamReader(response.GetResponseStream()))
+                {
+                    Response.Write(responseStream.ReadToEnd());
+                }
+            }
+        }
+    }
+}
