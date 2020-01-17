bc415708b751b859dacd531359bcdfbe0416ea85
Niraj <niraj.rules@gmail.com>
2020-01-17 20:59:29 +0000

    Updated Labs

---
+++ 08Microservices/webapplication/src/SampleIISApp/SetVariables.ps1
@@ -0,0 +1,11 @@
+﻿foreach($key in
+[System.Environment]::GetEnvironmentVariables('Process').Keys) {
+if ([System.Environment]::GetEnvironmentVariable($key, 'Machine') -eq
+$null) {
+$value = [System.Environment]::GetEnvironmentVariable($key,
+'Process')
+[System.Environment]::SetEnvironmentVariable($key, $value,
+'Machine')
+}
+}
+C:\ServiceMonitor.exe W3SVC
