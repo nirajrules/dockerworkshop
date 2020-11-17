3bebcc0274cf554ce28ddd0c84f779ee70e6989d
Niraj Bhatt <nirajbhatt@NB-MBPt13-2018.lan>
2020-11-17 13:25:40 -0500

    added interlock sample

---
+++ 17InterlockSample/src/Data/WeatherWebContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WeatherWeb.Models
+{
+    public class WeatherWebContext : DbContext
+    {
+        public WeatherWebContext (DbContextOptions<WeatherWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WeatherWeb.Models.WeatherData> WeatherData { get; set; }
+    }
+}
---
+++ 17InterlockSample/src/Pages/Weather/WeatherData.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using WeatherWeb.Models;
+
+namespace WeatherWeb.Pages.Weather
+{
+    public class WeatherDataModel : PageModel
+    {
+        private readonly WeatherWeb.Models.WeatherWebContext _context;
+
+        public WeatherDataModel(WeatherWeb.Models.WeatherWebContext context)
+        {
+            _context = context;
+        }
+
+        public IList<WeatherData> WeatherData { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("http://apiservice.kubernetes-dashboard.svc.cluster.local:32321/weatherforecast"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    WeatherData = JsonConvert.DeserializeObject<List<WeatherData>>(apiResponse);
+                }
+            }
+        }
+    }
+}
